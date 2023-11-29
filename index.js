a3e1667f4b4f57c75c738e829faea032cb5eed58
fakeistiak <istiakahmedb100.com>
2023-11-30 00:23:40 +0600

    admin dashboard done

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -26,38 +27,60 @@ async function run() {
     const userCollection = client.db("ficrosoft").collection("users");
     const worksheetCollection = client.db("ficrosoft").collection("worksheet");
 
+    // jwt related
 
-    // users related api 
-    app.get('/users', async (req, res) => {
-      const result = await userCollection.find().toArray();
-      res.send(result)
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
     });
 
-    app.post('/users', async(req, res) =>{ 
+    // middleware
+
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "forbidden access" });
+      }
+      const token = req.headers.authorization.split("")[1];
+
+      // next();
+    };
+
+    // users related api
+    app.get("/users", verifyToken, async (req, res) => {
+      console.log(req.headers);
+
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
       res.send(result);
-    })
+    });
 
-
-    app.patch('/users/admin/:id', async (req, res) => {
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
-          role: 'HR'
-        }
-      }
+          role: "HR",
+        },
+      };
       const result = await userCollection.updateOne(query, updatedDoc);
       res.send(result);
-    })
+    });
 
-    app.delete('/users/:id', async (req, res) => {
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
       res.send(result);
-    }	)
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
@@ -79,8 +102,25 @@ async function run() {
       }
     });
     //  for getting all the users
+    // app.get("/user", async (req, res) => {
+    //   try {
+    //     const result = await userCollection.find().toArray();
+    //     console.log(result);
+    //     res.send(result);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // });
+
+    //  for getting all the users and single user --NEW
     app.get("/user", async (req, res) => {
       try {
+        const email = req.query.email;
+        if (email) {
+          const query = { email };
+          const result = await userCollection.findOne(query);
+          return res.send(result);
+        }
         const result = await userCollection.find().toArray();
         console.log(result);
         res.send(result);
@@ -94,9 +134,26 @@ async function run() {
     app.get("/employee", async (req, res) => {
       try {
         const result = await userCollection
-          .find({ role: "employee" })
+          .find({ role: "Employee" })
           .toArray();
         console.log(result);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    app.patch("/employee/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        console.log(id);
+
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: { isVerified: true },
+        };
+
+        const result = await userCollection.updateOne(filter, updatedDoc);
         res.send(result);
       } catch (error) {
         console.log(error);
@@ -124,14 +181,10 @@ async function run() {
 
     app.get("/worksheet", async (req, res) => {
       const email = req.query.email;
-      const query={email: email};
+      const query = { email: email };
       const result = await worksheetCollection.find(query).toArray();
       res.send(result);
     });
-
-
-
-
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
